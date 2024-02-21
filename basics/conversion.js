c54b7c5f66fb2c98d9660a109961bcfccbf2d7f8
Nakul <74403617+Nakul00@users.noreply.github.com>
2024-02-21 15:50:55 +0000

    commit

---
+++ basics/conversion.js
@@ -0,0 +1,11 @@
+let score = 33
+console.log(typeof score);
+
+let valueInString = String(score)
+console.log(typeof valueInString);
+
+console.log(valueInString);
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1, false -> 0
