a861be3bbd8173dcaebe4e527c8ca114924e1ba9
Nakul <74403617+Nakul00@users.noreply.github.com>
2024-02-22 20:12:29 +0000

    commit

---
+++ basics/dates.js
@@ -0,0 +1,14 @@
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+
+console.log(typeof myDate);
+
+let mynewDate = new Date(2023, 0 , 23)
+console.log(mynewDate.toDateString());
+console.log(mynewDate.getTime());
+
+let mynewDate1 = new Date(2000, 1 , 26)
+console.log(mynewDate1.toDateString());
+
